b95ea35fb8a9fa8a786b2dcfb7976a043ee347a4
misraved <ved@turbot.com>
2022-09-07 03:24:25 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19

--- vercel/table_vercel_project.go
+++ vercel/table_vercel_project.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/chronark/vercel-go/endpoints/project"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func tableVercelProject(ctx context.Context) *plugin.Table {
