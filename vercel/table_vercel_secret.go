b1a2982b940c2f83f76d17eea72ed9d2af54ece3
Ved misra <47312748+misraved@users.noreply.github.com>
2022-09-09 01:10:21 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19 (#2)

--- vercel/table_vercel_secret.go
+++ vercel/table_vercel_secret.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/chronark/vercel-go/endpoints/secret"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func tableVercelSecret(ctx context.Context) *plugin.Table {
