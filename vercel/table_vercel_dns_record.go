be327053c8abec8df9065af5e84f0b86a986410c
misraved <ved@turbot.com>
2023-03-20 13:03:17 +0530

    Recompile plugin with steampipe-plugin-sdk v5.3.0

--- vercel/table_vercel_dns_record.go
+++ vercel/table_vercel_dns_record.go
@@ -6,9 +6,9 @@ import (
 	"github.com/chronark/vercel-go/endpoints/dns"
 	"github.com/chronark/vercel-go/endpoints/domain"
 
-	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
 
 func tableVercelDnsRecord(ctx context.Context) *plugin.Table {
